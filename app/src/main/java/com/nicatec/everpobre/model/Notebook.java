cbc4cccb1382588575650b1b3c527dfa410764ca
Vicente de Miguel <spidvmp@gmail.com>
2016-04-18 22:27:44 +0200

    Creo el modelo de notebook

---
+++ app/src/main/java/com/nicatec/everpobre/model/Notebook.java
@@ -0,0 +1,26 @@
+package com.nicatec.everpobre.model;
+
+/**
+ * Created by vtx on 18/4/16.
+ */
+public class Notebook {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
